312951a734b1016aaef65edd52e98a7dcb47e59d
cegarc5579 <carlos_garcia1@baylor.edu>
2022-02-16 13:02:58 -0600

    changes made to plant

--- a_PlantClass.py
+++ a_PlantClass.py
@@ -1,17 +1,24 @@
-
 class Plant:
-    def __init__(self,color):
+    # one attribute called color
+    def __init__(self, color):
         self.__color = color
 
-
+    # get method that returns color of the plant
     def get_color(self):
         return self.__color
 
 
-class Flower(Plant):
-    def __init__(self,color, petals):
-        Plant.__init__(self,color)
+# specialized version of a flower. flower inherits everything from Plant class
+# put Plant in paranthesis to show that ur inheriting everything
+# there is 2 attributes
+# color is needed to initialize the superclass
 
+
+class Flower(Plant):
+    def __init__(self, color, petals):
+        # create a plant first because it inherits from the superclass
+        Plant.__init__(self, color)
+        # petals only belongs to the subclass
         self.__petals = petals
 
     def get_petals(self):
--- a_PlantProgram.py
+++ a_PlantProgram.py
@@ -1,13 +1,21 @@
 import a_PlantClass as pc
 
+# creating instances of the class
+# primrose is a plant, and give it a color to create a instance
+# this will just create a plant called primrose
 primrose = pc.Plant("Green")
 
-sunflower = pc.Flower("Yellow")
-
+# creating this next one directly in subclass
+sunflower = pc.Flower("Yellow", 12)
+# for flower you need to give it two attributes because two attributes was set in class
+# or else it gives error
 print(primrose.get_color())
 
+# can still do get_color because it inherits from it
+
 print(sunflower.get_color())
+# this method is in subclass so it can be called
 print(sunflower.get_petals())
 
-
-print(primrose.get_petals())
+# get_petals is only in sub class and super class does not have access to this method
+# print(primrose.get_petals())
